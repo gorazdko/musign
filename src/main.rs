cb1e8e75b1741e891ca5ceffe399590b9e0208ec
gorazdko <gorazdko@gmail.com>
2021-02-27 19:16:46 +0100

    step

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,52 @@
 use clap::{Clap, ValueHint};
 use std::path::PathBuf;
+use std::str::FromStr;
 extern crate hex;
 extern crate secp256k1;
+use secp256k1::bitcoin_hashes::sha256;
 
 //use secp256k1::rand::rngs::OsRng;
 use secp256k1::rand::rngs::OsRng;
-use secp256k1::{PublicKey, Secp256k1, SecretKey};
+use secp256k1::{Message, PublicKey, Secp256k1, SecretKey, Signature};
 
 #[derive(Clap, Debug, PartialEq)]
-enum Signature {
+enum SigType {
     ECDSA,
     Schnorr,
 }
 
-fn generate_keypair(seed: Vec<u8>, _sig_type: Signature) -> (SecretKey, PublicKey) {
+fn generate_keypair(seed: Vec<u8>, _sig_type: SigType) -> (SecretKey, PublicKey) {
     let secp = Secp256k1::new();
     let secret_key = SecretKey::from_slice(&seed).expect("seed should be 32 bytes (64 characters)");
     let public_key = PublicKey::from_secret_key(&secp, &secret_key);
     (secret_key, public_key)
+}
+
+fn sign(seckey: String, msg: String, _sig_type: SigType) -> bool {
+    let seckey =
+        SecretKey::from_str(&seckey).expect("Private key must be 64 chars long hex string");
+
+    let message = Message::from_hashed_data::<sha256::Hash>(msg.as_bytes());
+    let secp = Secp256k1::new();
+    let sig = secp.sign(&message, &seckey);
+    let public_key = PublicKey::from_secret_key(&secp, &seckey);
+    assert!(secp.verify(&message, &sig, &public_key).is_ok());
+
+    true
+}
+
+fn verify(pubkey: String, msg: String, signature: String, _sig_type: SigType) -> bool {
+    let pubkey = PublicKey::from_str(&pubkey).expect("Public key must be 65 chars long hex string");
+    let sig = Signature::from_str(&signature).expect("Signature format incorrect");
+
+    let message = Message::from_hashed_data::<sha256::Hash>(msg.as_bytes());
+    let secp = Secp256k1::new();
+
+    if secp.verify(&message, &sig, &pubkey).is_ok() {
+        true
+    } else {
+        false
+    }
 }
 
 #[derive(Clap, Debug)]
@@ -30,7 +59,7 @@ enum Opt {
         seed: String,
         /// Purpose
         #[clap(arg_enum, default_value = "ecdsa")]
-        sig_type: Signature,
+        sig_type: SigType,
     },
 
     /// Sign
@@ -44,9 +73,12 @@ enum Opt {
         /// File to sign
         #[clap(name = "FILE", parse(from_os_str), value_hint = ValueHint::AnyPath)]
         file: PathBuf,
+        /// Message string to sign. Must be 32 bytes.
+        #[clap(conflicts_with = "FILE", short = 'm')]
+        msg_string: Option<String>,
         /// Signature type
         #[clap(arg_enum, default_value = "ecdsa")]
-        sig_type: Signature,
+        sig_type: SigType,
     },
     /// Verify
     Verify {
